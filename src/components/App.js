d6934662a852a165a63642374c23d5bd461c21b6
aggreyrc <aggreyronoh@gmail.com>
2024-08-14 01:04:51 +0300

    Fetching books

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 import Header from './Header';
+import BookList from './BookList';
 
 function App() {
 
@@ -10,13 +11,18 @@ function App() {
   useEffect(() => {
     fetch('https://gutendex.com/books/')
      .then(response => response.json())
-     .then(books => setBooks(books))
+     .then(data => setBooks(data.results))
      .catch(error => console.error('Error:', error))
   }, []);
+
 
   return (
     <div className="App">
       <Header />
+      <BookList
+      books = {books}
+      />
+  
     </div>
   );
 }
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
+import Search from "./Search";
 
-function () {
+function BookList ({books}) {
+
+      //Searching for books by author or title
+    //   const [searchTerm, setSearchTerm] = useState("")
+
+    //   function handleSearch(event){
+    //     setSearchTerm(event.target.value);
+    //   }
+
+    //   const filteredBooks = books.filter((book) => {
+    //     if(searchTerm ===""){
+    //         return true;
+    //     }else{
+    //         return book.title.toLowerCase().includes(searchTerm.toLowerCase()) || 
+    //         book.authors[0].name.toLowerCase().includes(searchTerm.toLowerCase()); 
+    //     }
+    //   })
+
     return(
         <div>
+            <Search handleSearch={handleSearch} />
             <h2>Book List</h2>
+            <ul className="book-list">
+            {filteredBooks.slice(0,12).map(book => (
+                <div key={book.id}>
+                    <img src={book.formats["image/jpeg"]} alt={book.title} />
+                    <h3>{book.title}</h3>
+                    <p>Author: {book.authors[0].name}</p>
+                    <p>Download: <a href={book.formats["application/x-mobipocket-ebook"]}>Download</a></p>
+                </div>
+                ))};
+                </ul>
         </div>
     )
 }
+
+export default BookList;
